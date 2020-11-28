f360e76efcd154d867418593e98af88261d43d01
NookieNoo <biv0208@yandex.ru>
2020-11-29 00:14:55 +0300

    Переделали на redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './styles.css'
-import {createStore} from './createStore'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
 import {rootReducer} from './redux/rootReducer'
+import { asyncIncrement, changeTheme, decrement, increment } from './redux/actions'
+import logger from 'redux-logger'
+
 
 const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
@@ -8,29 +12,54 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-const store = createStore(rootReducer, 0);
+
+// function logger (state) {
+//     return function(next) {
+//         return function(action) {
+//             console.log('prevState', state.getState())
+//             console.log('action', action);
+//             const newState = next(action)
+//             console.log('new state', newState)
+//             return newState
+//         }
+//     }
+// }
+
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+);
 
 window.store = store;
 
 addBtn.addEventListener('click', () => {
-    store.dispatch({type: 'INCREMENT'})
+    store.dispatch(increment())
 })
 subBtn.addEventListener('click', () => {
-    store.dispatch({type: 'DECREMENT'})
+    store.dispatch(decrement())
 })
 asyncBtn.addEventListener('click', () => {
-
+    store.dispatch(asyncIncrement())
 })
+
+
+themeBtn.addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light')
+    ? 'dark'
+    : 'light'
+    store.dispatch(changeTheme(newTheme))
+})
+
 
 store.subscribe(() => {
     const state = store.getState()
 
-    counter.textContent  = state;
+    counter.textContent  = state.counter;
+    document.body.className = state.theme.value;
+    [addBtn, subBtn, themeBtn, asyncBtn].forEach(btn => {
+        btn.disabled = state.theme.disabled
+    })
 })
 
 store.dispatch({type: 'INIT_APP'})
-
-
-themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark')
-})
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,35 @@
-export function rootReducer(state, action) {
-    if (action.type === 'INCREMENT') {
+import { combineReducers } from 'redux'
+import {ENABLE_BUTTONS, INCREMENT, DISABLE_BUTTONS} from './types'
+import {DECREMENT, CHANGE_THEME} from './types'
+
+function counterReducer(state = 0, action) {
+    if (action.type === INCREMENT) {
         return state + 1
-    } else if (action.type === "DECREMENT") {
+    } else if (action.type === DECREMENT) {
         return state - 1
     }
 
     return state
 }
+
+const initialThemeState = {
+    value: 'light',
+    disabled: false
+}
+
+function themeReducer(state = initialThemeState, action) {
+    switch (action.type) {
+        case CHANGE_THEME:
+            return {...state, value: action.payload}
+        case ENABLE_BUTTONS:
+            return {...state, disabled: false}
+        case DISABLE_BUTTONS:
+            return {...state, disabled: true}
+        default: return state
+    }
+}
+
+export const rootReducer = combineReducers({
+    counter: counterReducer,
+    theme: themeReducer
+})
